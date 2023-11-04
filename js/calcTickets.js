8a26cabc0571695f4821f9bcd2f4ad4dafd3bc1c
Ian Azariel Oikawa Regojo <oikawa1440@gmail.com>
2023-11-04 16:05:54 -0300

    agregado VentaTickets

---
+++ js/calcTickets.js
@@ -0,0 +1,34 @@
+const PRECIO = 200;
+const ESTUDIANTE = 0.2;
+const TRAINEE = 0.5;
+const JUNIOR = 0.85;
+
+function calculoPrecio() {
+let cant = document.getElementById("fcant").value;
+let cat = document.getElementById("categorias").value;
+let total;
+document.getElementById("tot").innerHTML = ("")
+if (isNaN(cant) || cant < 0) {
+    console.log(`ingrese un valor valido`);
+}
+if(cat === "Estudiante") {
+    total=(cant*PRECIO)*ESTUDIANTE;
+}
+else if (cat === "Trainee") {
+    total=(cant*PRECIO)*TRAINEE;
+}
+else if (cat === "Junior") {
+    total=(cant*PRECIO)*JUNIOR;
+}
+document.getElementById("tot").innerHTML += (`${total}`)
+console.log(`El total es ${total}`);
+}
+
+
+function borrar() {
+    document.getElementById("nom").value = ("");
+    document.getElementById("ap").value = ("");
+    document.getElementById("email").value = ("");
+    document.getElementById("fcant").value = ("");
+    document.getElementById("tot").innerHTML = ("");
+}
